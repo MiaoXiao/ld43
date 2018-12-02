3f67f37ef066c6c865d9b152b1d3f58f6445ba2c
dzhan008 <dzhan008@ucr.edu>
2018-12-02 13:18:58 -0800

    Big eggs cannot fit into small keyholes anymore

--- ld43/Assets/Scripts/Player.cs
+++ ld43/Assets/Scripts/Player.cs
@@ -30,25 +30,34 @@ public class Player : MonoBehaviour
             if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, range, layerMask))
             {
                 GameObject hitObject = hit.transform.gameObject;
-                Debug.Log(hitObject.name);
+                Debug.Log(hitObject.tag);
                 if (hitObject.tag == "Egg")
                 {
                     if (hitObject.GetComponent<Egg>().sourceKeyhole)
                         hitObject.GetComponent<Egg>().removeFromKeyHole();
                     playerInventory.AddItem(hitObject.GetComponent<Egg>());
                     //Debug.DrawRay(rayOrigin, playerCamera.transform.forward * hit.distance, Color.white);
-                    Debug.Log("Hit!");
+                    //Debug.Log("Hit!");
                 }
-                else if (hitObject.tag == "Keyhole" && playerInventory.currentlySelected != null && !hitObject.GetComponent<Keyhole>().eggInserted)
+                else if (hitObject.tag == "Keyhole")
                 {
                     Debug.Log(playerInventory.currentlySelected.inFrontOfPlayerObj.GetComponent<Egg>());
-                    playerInventory.currentlySelected.inFrontOfPlayerObj.GetComponent<Egg>().placeInKeyHole(hitObject.GetComponent<Keyhole>());
-                    playerInventory.RemoveItem(playerInventory.currentlySelected.inFrontOfPlayerObj.GetComponent<Egg>());
+                    Debug.Log(hitObject.GetComponent<Keyhole>().eggInserted);
+                    if (playerInventory.currentlySelected != null && !hitObject.GetComponent<Keyhole>().eggInserted)
+                    {
+                        //If we can place the current egg in the keyhole, remove it from the inventory.
+                        if (playerInventory.currentlySelected.inFrontOfPlayerObj.GetComponent<Egg>().placeInKeyHole(hitObject.GetComponent<Keyhole>()))
+                        {
+                            Debug.Log("Removing item!");
+                            playerInventory.RemoveItem(playerInventory.currentlySelected.inFrontOfPlayerObj.GetComponent<Egg>());
+                        }
+
+                    }
                 }
                 else
                 {
                     Debug.DrawRay(rayOrigin, playerCamera.transform.forward * hit.distance, Color.blue);
-                    Debug.Log("Hit something that's not relevant..");
+                    //Debug.Log("Hit something that's not relevant..");
                 }
             }
             else
