a9799724527a2e36f9c0944b9d64540e2bb7ee46
dzhan008 <dzhan008@ucr.edu>
2018-12-01 18:01:08 -0800

    Added raycasting for interaction

--- ld43/Assets/Scripts/Player.cs
+++ ld43/Assets/Scripts/Player.cs
@@ -11,8 +11,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Egg testEgg;
 
+    public Transform raycastPoint;
+    public Camera playerCamera;
+    private Vector3 rayOrigin;
+    public int range;
+
     private void Awake()
     {
+        playerInventory.AddItem(testEgg);
+        playerInventory.AddItem(testEgg);
+        playerInventory.AddItem(testEgg);
         playerInventory.AddItem(testEgg);
     }
 
@@ -24,7 +32,21 @@ public class Player : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.E)) //interact
         {
-
+            rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, range))
+            {
+                if(hit.transform.gameObject.tag == "Egg")
+                {
+                    Debug.DrawRay(rayOrigin, transform.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+                    Debug.Log("Hit!");
+                }
+            }
+            else
+            {
+                Debug.DrawRay(rayOrigin, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+                Debug.Log("Missed..");
+            }
         }
 	}
 }
